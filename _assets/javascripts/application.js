c31486f1ffb99aaa3d46b0ae771eb87d735067b8
Tom Richards <tom@tomrichards.net>
2017-03-07 03:07:23 +0000

    Apply js standard style

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,20 +1,20 @@
-/* global $, lity */
+/* global jQuery, lity */
 
-(function($) {
+(function ($) {
   var lightbox = null
 
   $('.video-link').on('click', function (evt) {
     evt.preventDefault()
     var link = $(evt.currentTarget)
     lightbox = lity(`
-    <video autoplay controls onended="handleEnded()">
-      <source src="${link.attr('href')}" type="video/mp4">
-    </video>
+      <video autoplay controls onended="handleEnded()">
+        <source src="${link.attr('href')}" type="video/mp4">
+      </video>
     `) //
     return false
   })
 
-  window.handleEnded = function() {
+  window.handleEnded = function () {
     console.log('video ended!')
     lightbox.close()
   }
