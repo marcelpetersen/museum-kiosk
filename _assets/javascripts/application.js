5b889c9b8fd46fd60b35348850893bd4a8225714
Tom Richards <tom@tomrichards.net>
2017-03-07 02:53:21 +0000

    Add sample video and images

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,3 +1,21 @@
-(function() {
-    console.log('Hello!')
-})()
+/* global $, lity */
+
+(function($) {
+  var lightboxContent = function (videoSrc) {
+    return
+  }
+
+  var lightbox = null
+
+  $('.video-link').on('click', function (evt) {
+    evt.preventDefault()
+    var link = $(evt.currentTarget)
+    lightbox = lity(`<video src="${link.attr('href')}" autoplay controls />`)
+    return false
+  })
+
+  $(document).on('ended', function(evt) {
+    console.log('video ended!')
+    lightbox.close()
+  })
+})(jQuery)
--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,4 +1,2 @@
 #= require jquery/dist/jquery.js
-#= require tether/dist/js/tether.js
-#= require bootstrap/dist/js/bootstrap.js
 #= require lity/dist/lity.js
