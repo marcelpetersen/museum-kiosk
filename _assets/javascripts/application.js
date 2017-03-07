fe54fd8e01bcf479cd0f00473f1618a243988448
Tom Richards <tom@tomrichards.net>
2017-03-07 03:03:49 +0000

    Close lightbox when video ends

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,21 +1,21 @@
 /* global $, lity */
 
 (function($) {
-  var lightboxContent = function (videoSrc) {
-    return
-  }
-
   var lightbox = null
 
   $('.video-link').on('click', function (evt) {
     evt.preventDefault()
     var link = $(evt.currentTarget)
-    lightbox = lity(`<video src="${link.attr('href')}" autoplay controls />`)
+    lightbox = lity(`
+    <video autoplay controls onended="handleEnded()">
+      <source src="${link.attr('href')}" type="video/mp4">
+    </video>
+    `) //
     return false
   })
 
-  $(document).on('ended', function(evt) {
+  window.handleEnded = function() {
     console.log('video ended!')
     lightbox.close()
-  })
+  }
 })(jQuery)
