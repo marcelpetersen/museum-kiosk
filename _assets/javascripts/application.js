64c47e564e63f1f41240af9388b2a44d28623e7d
Tom Richards <tom@tomrichards.net>
2017-03-07 03:26:37 +0000

    Abandon template strings because they break execjs

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,16 +1,15 @@
 /* global jQuery, lity */
-
 (function ($) {
   var lightbox = null
+
+  var lightboxContent = function (videoUrl) {
+    return '<video autoplay controls onended="handleEnded()"><source src="' + videoUrl + '" type="video/mp4"></video>'
+  }
 
   $('.video-link').on('click', function (evt) {
     evt.preventDefault()
     var link = $(evt.currentTarget)
-    lightbox = lity(`
-      <video autoplay controls onended="handleEnded()">
-        <source src="${link.attr('href')}" type="video/mp4">
-      </video>
-    `) //
+    lightbox = lity(lightboxContent(link.attr('href')))
     return false
   })
 
